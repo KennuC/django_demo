a2e19b8333076170b48d63907c3be4726352cb00
KennuC <131323586+KennuC@users.noreply.github.com>
2023-08-26 09:26:08 +1000

    Add files via upload

---
+++ django_demo/mysite/polls/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+from .models import Choice, Question
+
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    extra = 1
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes":["collapse"]}),
+    ]
+    inlines = [ChoiceInLine]
+
+
+# Register your models here.
+admin.site.register(Question, QuestionAdmin)
